364515016225d3f0c22f30515625763895bf4c53
Cristian Estarlich <cestarlich.dev@gmail.com>
2025-03-23 23:12:23 +0100

    More and more configs and tooling, introducing tailwind

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   root: true,
   extends: ['@cristian-starter-kit/eslint-config'],
-} 
+}
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,13 +1,13 @@
-import express from "express";
+import express from 'express'
 
-const app = express();
-const port = process.env.PORT || 3001;
+const app = express()
+const port = process.env.PORT || 3001
 
-app.get("/", (req, res) => {
-  const cristian = "Cristian";
-  res.json({ message: "Hello World!" });
-});
+app.get('/', (req, res) => {
+  const cristian = 'Cristian'
+  res.json({ message: 'Hello World!' })
+})
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+  console.log(`Server running at http://localhost:${port}`)
+})
